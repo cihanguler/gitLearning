f53ebd5d01ebd1ac9e63a3b6c113285093adabf1
cihanguler <cihanguler0049@gmail.com>
2020-05-06 20:22:38 +0200

    pushLeran.java

---
+++ src/tests/pushLeran.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class pushLeran {
+
+    public static void main(String[] args) {
+
+        System.out.println("push learning");
+        System.out.println("push learning");
+    }
+}
