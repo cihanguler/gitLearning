02a916514295815320104f69cc8b4b6f6e5fae91
cihanguler <cihanguler0049@gmail.com>
2020-05-06 17:22:10 +0200

    update sprint

--- src/tests/sprint.java
+++ src/tests/sprint.java
@@ -2,6 +2,12 @@ package tests;
 
 public class sprint {
     public static void main(String[] args) {
-        System.out.println("gdhdhfdgjh");
+        System.out.println("gdhd.....hfdgjh");
+
+
+        System.out.println("sdfgsjhsdklgsdhgşk");
+
+
+
     }
 }
