9576f972d24c843a998c358b08bfc4f488af1232
Cihan GÜLER <58997250+cihanguler@users.noreply.github.com>
2020-05-08 01:54:36 +0200

    Update Login.java

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -5,5 +5,7 @@ public class Login {
     public static void main(String[] args) {
 
         System.out.println("erased the old one and written new code...");
+        
+        //added this line from Github
     }
 }
